7e8c9486256f66d64183153330826bcb6b20cbc6
Emanuele Mazzante <emanuele78m@gmail.com>
2019-03-18 17:43:46 +0100

    Aggiunta migration per tabella users / rimossa migration password reset

--- class3-project/database/migrations/2014_10_12_000000_create_users_table.php
+++ class3-project/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,12 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
+            $table->increments('id');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('date_of_birth')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
