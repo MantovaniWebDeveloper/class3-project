b2b404addb56500d936b2abdcee714eb02e8e6d2
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-03-19 10:30:57 +0100

    header interno perfettamente posizionato

--- class3-project/resources/views/header.blade.php
+++ class3-project/resources/views/header.blade.php
@@ -1,8 +1,8 @@
-<header>
+<header id="headerInterno">
   <div class="container">
     <div class="row">
       <div class="wrapLogo col-6">
-        <h1>Bool BNB</h1>
+        <h1 class="logo_title">BoolBNB</h1>
       </div>
       <div class="wrapMenu col-6">
         <i class="fas fa-bars"></i>
--- class3-project/resources/views/result.blade.php
+++ class3-project/resources/views/result.blade.php
@@ -1,4 +1,15 @@
 @extends('layout.main')
 @section('content')
 @include('header')
+<div class="wrapSearch">
+  <div class="container">
+    <form action="">
+      <input type="text" name="" value="">
+      <select class="" name="">
+        <option value=""></option>
+      </select>
+      <input type="submit" name="" value="">
+    </form>
+  </div>
+</div>
 @endsection
