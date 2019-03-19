9082a8d5c933068dd03e6e4c4a3e7e32a17945b5
Emanuele Mazzante <emanuele78m@gmail.com>
2019-03-19 12:24:18 +0100

    Merge di web.php

--- class3-project/routes/web.php
+++ class3-project/routes/web.php
@@ -1,20 +1,4 @@
 <?php
-<<<<<<< HEAD
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', 'HomeController@index')->name('home');
-Route::get('/result', 'HomeController@result')->name('result');
-=======
 	
 	/*
 	|--------------------------------------------------------------------------
@@ -26,7 +10,21 @@ Route::get('/result', 'HomeController@result')->name('result');
 	| contains the "web" middleware group. Now create something great!
 	|
 	*/
+	//test dario
+	Route::get('/', 'HomeController@index')->name('home');
+	Route::get('/result', 'HomeController@result')->name('result');
 	
+	/*
+	|--------------------------------------------------------------------------
+	| Web Routes
+	|--------------------------------------------------------------------------
+	|
+	| Here is where you can register web routes for your application. These
+	| routes are loaded by the RouteServiceProvider within a group which
+	| contains the "web" middleware group. Now create something great!
+	|
+	*/
+	//test emanuele
 	
 	Auth::routes();
 	
@@ -35,5 +33,5 @@ Route::get('/result', 'HomeController@result')->name('result');
 	//ROTTE DI TEST
 	Route::get('/appartamenti/ricerca', 'ApartmentController@simpleSearch');
 	Route::get('/', 'ApartmentController@index');
-	Route::get('/token','ApartmentController@showToken');
->>>>>>> emanuele
+	Route::get('/token', 'ApartmentController@showToken');
+
