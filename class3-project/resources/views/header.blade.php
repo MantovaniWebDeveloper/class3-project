be7455a5909abc3f05d79858c48052a45016c69b
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-03-19 10:20:50 +0100

    fatta la prima versione layoutgenerale e componente heder scheletro

---
+++ class3-project/resources/views/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+  <div class="container">
+    <div class="row">
+      <div class="wrapLogo col-6">
+        <h1>Bool BNB</h1>
+      </div>
+      <div class="wrapMenu col-6">
+        <i class="fas fa-bars"></i>
+      </div>
+    </div>
+  </div>
+</header>
