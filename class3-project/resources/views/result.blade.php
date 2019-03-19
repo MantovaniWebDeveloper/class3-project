987b28713c0df56617cea1710089b9d998fb8dd3
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-03-19 11:09:46 +0100

    inseriti i name corretti

--- class3-project/resources/views/result.blade.php
+++ class3-project/resources/views/result.blade.php
@@ -5,6 +5,22 @@
   <div class="container">
     @include('search')
     <h4>Filtra per :</h4>
+
+    <h4>Includi anche :</h4>
+    <div class="row">
+      <div class="col-3">
+        <input type="checkbox" name="service" ><label class="nomiCheck">Parcheggio</label>
+      </div>
+      <div class="col-3">
+        <input type="checkbox" name="service"><label class="nomiCheck">Wi-fi</label>
+      </div>
+      <div class="col-3">
+        <input type="checkbox" name="service"> <label class="nomiCheck">Cancellazione gratuita</label>
+      </div>
+      <div class="col-3">
+        <input type="checkbox" name="service"><label class="nomiCheck">Piscina</label>
+      </div>
+    </div>
   </div>
 </div>
 @endsection
--- class3-project/resources/views/search.blade.php
+++ class3-project/resources/views/search.blade.php
@@ -2,11 +2,16 @@
   <form class="" action="index.html" method="post">
     <div class="form-row d-flex justify-content-center">
     <div class="form-group col-3">
-      <input type="text" class="form-control"  placeholder="Localita">
+      <input type="text" class="form-control"  name="city" placeholder="Localita">
     </div>
     <div class="form-group col-3">
-      <select class="selectPersone form-control" name="">
+      <select class="selectPersone form-control" name="min_beds">
         <option value="">numero persone</option>
+      </select>
+    </div>
+    <div class="form-group col-3">
+      <select class="selectPersone form-control" name="min_rooms">
+        <option value="">numero stanze</option>
       </select>
     </div>
   </div>
