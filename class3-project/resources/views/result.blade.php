3188a309dc6a0b4667301830c5490a50cb2fa563
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-03-21 17:18:23 +0100

    aggiunta sfondo, img cordi nei resutl ecc

--- class3-project/resources/views/result.blade.php
+++ class3-project/resources/views/result.blade.php
@@ -2,26 +2,32 @@
 @section('content')
 @include('header')
 <div class="wrapSearch">
-  <div class="container">
     @include('searchInterno')
     <div class="wrapAll">
-      <aside>
+      <div class="container">
+        <div class="wrapAllInterno">
+          <aside>
 
-      </aside>
-      <div class="wrapResult">
-        @foreach ($apartments as  $appartamento)
-          <div class="row ">
-            <div class="col-4">
+          </aside>
+          <div class="wrapResult">
+            @foreach ($apartments as  $appartamento)
+                <div class="wrapRisultato">
+                  <div class="row ">
+                  <div class="col-4">
+                    <img class="img-fluid"src="{{asset("img/")}}/{{$appartamento->images()->first()->path}}" alt="">
+                  </div>
+                  <div class="col-6">
+                    <h5>{{$appartamento->title}}</h5>
+                    <h5>numeri letto: {{$appartamento->bed_count}}</h5>
+                    <p>{{ $appartamento->description}}</p>
+                  </div>
+                </div>
 
-            </div>
-            <div class="col-6">
-              <h3>{{$appartamento->title}}</h3>
-            </div>
-          </div>
-        @endforeach
-  </div>
+              </div>
+            @endforeach
+        </div>
+      </div>
     </div>
-  </div>
 </div>
 
 <script src="{{ asset('js/ricercaInterna.js') }}" defer></script>
