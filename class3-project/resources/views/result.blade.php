923edda409939217155973aa60045fd7ab8009dc
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-03-21 17:26:37 +0100

    aggiunto btn descrizione, corretto il col

--- class3-project/resources/views/result.blade.php
+++ class3-project/resources/views/result.blade.php
@@ -13,13 +13,17 @@
             @foreach ($apartments as  $appartamento)
                 <div class="wrapRisultato">
                   <div class="row ">
-                  <div class="col-4">
+                  <div class="col-4 wrap-img">
                     <img class="img-fluid"src="{{asset("img/")}}/{{$appartamento->images()->first()->path}}" alt="">
                   </div>
-                  <div class="col-6">
+                  <div class="col-8">
                     <h5>{{$appartamento->title}}</h5>
                     <h5>numeri letto: {{$appartamento->bed_count}}</h5>
                     <p>{{ $appartamento->description}}</p>
+                    <h3>{{ $appartamento->price}} €</h3>
+                    <div class="wrapBtn">
+                      <button class="btn btn-danger text-right"type="button" name="button">Descrizione completa</button>
+                    </div>
                   </div>
                 </div>
 
