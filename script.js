5ace745aefb87ffe288d09c3cce7284ac6bd851a
Sabrina Paola <sabripaola1808@gmail.com>
2019-03-22 00:23:04 +0100

    Aggiunto effetto hover

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
 $(document).ready(function(){
-    $('#burgher-menu').click(function(){
-        console.log('click');
-        $('#menu').toggleClass('visibile');
-    })
+    console.log('ready');
 })
