896587906c3c67ecbb9c5e2551e610a7fb6f9e3b
Sabrina Paola <sabripaola1808@gmail.com>
2019-03-21 23:49:56 +0100

    Aggiunto menu primary

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('#burgher-menu').click(function(){
         console.log('click');
-        $('#menu').toggleClass('block');
+        $('#menu').toggleClass('visibile');
     })
 })
