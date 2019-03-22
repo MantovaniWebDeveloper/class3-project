ba15543502622886ff7e4bb0a4b2fc4417b1973b
Sabrina Paola <sabripaola1808@gmail.com>
2019-03-22 10:56:46 +0100

    modificati input

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
     console.log('ready');
 });
+{
+    flatpickr("#check-in", {
+        altInput: true,
+        altFormat: "F j, Y",
+        dateFormat: "Y-m-d",
+        minDate: "today",
+    });
 
-
-flatpickr("#check-in", {
-    altInput: true,
-    altFormat: "F j, Y",
-    dateFormat: "Y-m-d",
-    minDate: "today",
-});
-
-flatpickr("#check-out", {
-    altInput: true,
-    altFormat: "F j, Y",
-    dateFormat: "Y-m-d",
-});
+    flatpickr("#check-out", {
+        altInput: true,
+        altFormat: "F j, Y",
+        dateFormat: "Y-m-d",
+        minDate: "today",
+    });
+}
