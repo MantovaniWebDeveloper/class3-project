85cc1393996f1e0074a64eac207032163d9deeb4
Sabrina Paola <sabripaola1808@gmail.com>
2019-03-22 10:45:34 +0100

    aggiunto datepicker

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
     console.log('ready');
-})\
+});
+
+
+flatpickr("#check-in", {
+    altInput: true,
+    altFormat: "F j, Y",
+    dateFormat: "Y-m-d",
+    minDate: "today",
+});
+
+flatpickr("#check-out", {
+    altInput: true,
+    altFormat: "F j, Y",
+    dateFormat: "Y-m-d",
+});
