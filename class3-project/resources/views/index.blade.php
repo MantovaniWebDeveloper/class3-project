b64131e0a7b3220a3c0de25d4df3e6aa26ff6205
davidepatruno <patrunodavid@gmail.com>
2019-03-25 10:04:59 +0100

    on-click cities parziale

--- class3-project/resources/views/index.blade.php
+++ class3-project/resources/views/index.blade.php
@@ -76,7 +76,7 @@
             <h3>Località</h3>
             <div class="box-card">
               <div class="card-città">
-                <div class="img-city">
+                <div class="img-city" onclick="location.href='appartamenti/ricerca?bed_count=0&{{city_code=5}}'">
                   <img src="http://lorempixel.com/250/250/nature" alt="camera">
                   <h4>Dubai</h4>
                 </div>
