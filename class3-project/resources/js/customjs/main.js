ac9760b8eadd4a32858965427643fcb072a42272
Emanuele Mazzante <emanuele78m@gmail.com>
2019-04-03 13:04:21 +0200

    Eliminati file inutilizzati

--- class3-project/resources/js/customjs/main.js
+++
@@ -1,7 +0,0 @@
-alert("prova");
-// $(document).ready(function(){
-//     $('#burgher-menu').click(function(){
-//         console.log('click');
-//         $('#menu').toggleClass('block');
-//     })
-// })
